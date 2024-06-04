5424dd4c8e2ac36e383300f519fcaa66b95bc3dd
zj kkk <npckei217@gmail.com>
2024-06-04 10:45:41 +0800

    feat: update

--- src/lib/@uniswap/uni-sdk/constants.d.ts
+++ src/lib/@uniswap/uni-sdk/constants.d.ts
@@ -1,37 +1,37 @@
-import JSBI from 'jsbi'
-export declare type BigintIsh = JSBI | bigint | string
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
 export declare enum ChainId {
-  MAINNET = 1,
-  BLAST = 81457,
-  TAIKO = 167000,
-  ARBITRUM_SEPOLIA = 421614
+    MAINNET = 1,
+    BLAST = 81457,
+    TAIKO = 167000,
+    ARBITRUM_SEPOLIA = 421614
 }
 export declare enum TradeType {
-  EXACT_INPUT = 0,
-  EXACT_OUTPUT = 1
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
 }
 export declare enum Rounding {
-  ROUND_DOWN = 0,
-  ROUND_HALF_UP = 1,
-  ROUND_UP = 2
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
 }
-export declare const FACTORY_ADDRESS = '0x643dA4b01FED3B245BE37E756c131052337E178a'
-export declare const INIT_CODE_HASH = '1892ee6b3b8f653471529d0b06a772bdc5588bb0b15607cb427c8148f70004a9'
-export declare const MINIMUM_LIQUIDITY: JSBI
-export declare const ZERO: JSBI
-export declare const ONE: JSBI
-export declare const TWO: JSBI
-export declare const THREE: JSBI
-export declare const FIVE: JSBI
-export declare const TEN: JSBI
-export declare const _100: JSBI
-export declare const _997: JSBI
-export declare const _1000: JSBI
+export declare const FACTORY_ADDRESS = "0x643dA4b01FED3B245BE37E756c131052337E178a";
+export declare const INIT_CODE_HASH = "0x1892ee6b3b8f653471529d0b06a772bdc5588bb0b15607cb427c8148f70004a9";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
 export declare enum SolidityType {
-  uint8 = 'uint8',
-  uint256 = 'uint256'
+    uint8 = "uint8",
+    uint256 = "uint256"
 }
 export declare const SOLIDITY_TYPE_MAXIMA: {
-  uint8: JSBI
-  uint256: JSBI
-}
+    uint8: JSBI;
+    uint256: JSBI;
+};
